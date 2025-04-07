d34d6b60a521114eab9de608b2edbdfaac3702db
agostinatravascio <atravascio@mail.austral.edu.ar>
2025-04-07 15:51:27 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,7 +3,6 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
-    txt = "12345678910"
-    print(txt[4::2])
+    txt = input()
     print(txt[4::2])
     
